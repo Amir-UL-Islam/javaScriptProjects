7a814d4ed245d6503cff08dc728593a735e7ecd2
Amir-UL <mangoleo51@gmail.com>
2020-04-14 21:48:28 +0600

    mathod-last-& final-fullfil

--- js/script.js
+++ js/script.js
@@ -21,9 +21,21 @@ function showSuccess(input) {
 }
 
 //--------------------checking the eamil
-function isValidEmail(eamil) {
+function checkEmail(input) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
+  if (re.test(input.value.trim())) {
+    showSuccess();
+  } else {
+    showError(input, 'Email is not valid');
+  }
+}
+
+//------------------check password match
+
+function checkPasswordsMatch(input1, input2) {
+  if (input1.value !== input2.value) {
+    showError(input2, 'Password do not matched');
+  }
 }
 //------------------check required fild
 
@@ -37,6 +49,21 @@ function checkRequired(inputArr) {
   });
 }
 
+//-------------------check length
+
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(input, `${getFileName(input)} must be at least ${min}characters`);
+  } else if (input.value.lenth > max) {
+    showError(
+      input,
+      `${getFileName(input)} must be at less than ${max}characters`
+    );
+  } else {
+    showSuccess(input);
+  }
+}
+
 //-------------------get file name
 
 function getFileName(input) {
@@ -48,4 +75,8 @@ function getFileName(input) {
 form.addEventListener('submit', function (event) {
   event.preventDefault();
   checkRequired([username, email, password, password2]);
+  checkLength(username, 3, 15);
+  checkLength(password, 7, 15);
+  checkEmail(email);
+  checkPasswordsMatch(password, password2);
 });
