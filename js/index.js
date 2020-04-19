e7f1554fd9ecfd2a58a9f39b72fb3df6f5db6d25
Amir-UL <mangoleo51@gmail.com>
2020-04-20 03:06:16 +0600

    adding js

---
+++ js/index.js
@@ -0,0 +1,14 @@
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('movie');
+
+const ticketPrice = +movieSelect.value;
+
+container.addEventListener('click', function (event) {
+    if (event.target.classList.contains('seat')) {
+
+    }
+
+});
