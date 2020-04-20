7ecd97533ae4b1e39807298cb2f2a780e77f43b7
Amir-UL <mangoleo51@gmail.com>
2020-04-21 05:07:37 +0600

    mathod one complete

--- js/index.js
+++ js/index.js
@@ -1,14 +1,37 @@
+// --------------collecting all element
+
 const container = document.querySelector('.container');
 const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
-const ticketPrice = +movieSelect.value;
+let ticketPrice = +movieSelect.value;
+
+//---------------------update selected count
+
+function updateSelectedCount() {
+  const selectedSeat = document.querySelectorAll('.row .seat.selected');
+  const selectedSeatCount = selectedSeat.length;
+  count.innerText = selectedSeatCount;
+  total.innerText = selectedSeatCount * ticketPrice;
+}
+
+//----------------------movie selected event
+
+movieSelect.addEventListener('change', function (event) {
+  ticketPrice = event.target.value;
+  updateSelectedCount();
+});
+
+// ---------------------adding event listener
 
 container.addEventListener('click', function (event) {
-    if (event.target.classList.contains('seat')) {
-
-    }
-
+  if (
+    event.target.classList.contains('seat') &&
+    !event.target.classList.contains('occupied')
+  ) {
+    event.target.classList.toggle('selected');
+    updateSelectedCount();
+  }
 });
