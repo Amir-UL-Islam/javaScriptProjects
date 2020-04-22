23deeabf923976c56c066425c6fee3b5d53d6482
Amir-UL <mangoleo51@gmail.com>
2020-04-23 01:14:10 +0600

    end of movie seat booking project also some correction

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@ const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
+populateUi();
 
 let ticketPrice = +movieSelect.value;
 
@@ -12,9 +13,18 @@ let ticketPrice = +movieSelect.value;
 
 function updateSelectedCount() {
   const selectedSeat = document.querySelectorAll('.row .seat.selected');
+
+  const seatIndex = [...selectedSeat].map(function (seat) {
+    return [...seats].indexOf(seat);
+  });
+  localStorage.setItem('selectedSeats', JSON.stringify(seatIndex));
+
+
   const selectedSeatCount = selectedSeat.length;
   count.innerText = selectedSeatCount;
   total.innerText = selectedSeatCount * ticketPrice;
+
+
 }
 
 //----------------------movie selected event
@@ -22,6 +32,13 @@ function updateSelectedCount() {
 movieSelect.addEventListener('change', function (event) {
   ticketPrice = event.target.value;
   updateSelectedCount();
+
+  function movieData(selectedIndex, price) {
+    localStorage.setItem('movieName', selectedIndex);
+    localStorage.setItem('moviePrice', price);
+  }
+  movieData(event.target.selectedIndex, event.target.value);
+
 });
 
 // ---------------------adding event listener
@@ -35,3 +52,22 @@ container.addEventListener('click', function (event) {
     updateSelectedCount();
   }
 });
+// --------------get data from localstorage and populate UI
+function populateUi() {
+  const selectedSeatsFromLocalS = JSON.parse(localStorage.getItem('selectedSeats'));
+  if (selectedSeatsFromLocalS !== null && selectedSeatsFromLocalS.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeatsFromLocalS.indexOf(index) > -1) {
+        seat.classList.add('selected');
+      }
+    });
+  }
+
+  const movieNameFromLocalS = localStorage.getItem('movieName');
+  if (movieNameFromLocalS !== null && movieNameFromLocalS > 0) {
+    movieSelect.selectedIndex = movieNameFromLocalS;
+  }
+  const moviePriceFromLocalS = localStorage.getItem('moviePrice');
+}
+//------------initialized count and total 
+updateSelectedCount();
