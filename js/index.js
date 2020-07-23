a69d431bcede290b703dafa55422041bdbafc662
Amir-UL <mangoleo51@gmail.com>
2020-07-24 01:30:42 +0600

    Done

--- js/index.js
+++ js/index.js
@@ -16,9 +16,44 @@ async function getRandonUser() {
     const user = data.results[0];
     const newUser = {
         name: `${user.name.first} ${user.name.last}`,
-        money: Math.floor(Math.random() * 1000000)
+        money: Math.floor(Math.random() * 100000)
     };
     addData(newUser);
+}
+
+// Find Millionaires
+
+function findMillionaires() {
+    data = data.filter(user => user.money > 10000000);
+    updateDOM();
+};
+
+// find the Total 
+
+function total() {
+    const Wealth = data.reduce((acc, user) => (acc += user.money), 0);
+    const wealthAfter = document.createElement('div');
+    wealthAfter.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(Wealth)}</strong></h3>`;
+    main.appendChild(wealthAfter);
+};
+
+
+// Sort Richest
+
+function sortRichest() {
+    data.sort((a, b) => b.money - a.money);
+    updateDOM();
+};
+// Doubles Money
+
+function doubleMoney() {
+    data = data.map((user) => {
+        return {
+            ...user,
+            money: user.money * 2
+        };
+    });
+    updateDOM();
 }
 // --add new data
 function addData(object) {
@@ -28,7 +63,7 @@ function addData(object) {
 //--update DOM
 function updateDOM(providedData = data) {
     //-- Clear the Main Div
-    main.innerHTML = ' <h2><strong>Person</strong>Wealth</h2> ';
+    main.innerHTML = ' <h2><strong>Person</strong>Wealth</> ';
     providedData.forEach(item => {
         const element = document.createElement("div");
         element.classList.add("person");
@@ -46,3 +81,7 @@ function formatMoney(number) {
 // Event Listener
 
 addUser.addEventListener('click', getRandonUser);
+makeDouble.addEventListener('click', doubleMoney);
+sort.addEventListener('click', sortRichest);
+millionaires.addEventListener('click', findMillionaires);
+calculate.addEventListener('click', total);
