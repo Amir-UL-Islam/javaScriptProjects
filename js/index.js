db82002e3ae4db3694e760d0a8ad440a67a1e763
Amir-UL <mangoleo51@gmail.com>
2020-11-13 01:53:41 +0600

    Build The Backbone of JS

---
+++ js/index.js
@@ -0,0 +1,33 @@
+// getting the DOM
+
+const navbar = document.querySelector(".navbar");
+const closeCartBtn = document.querySelector(".close-cart");
+const clearCartBtn = document.querySelector(".clear-cart");
+const cartDOM = document.querySelector(".card");
+const cartOverlay = document.querySelector(".cart-overlay");
+const cartItems = document.querySelector(".cart-items");
+const cartTotal = document.querySelector(".cart-total");
+const cartContent = document.querySelector(".cart-content");
+const productsDOM = document.querySelector(".products-center");
+//
+// const  = document.querySelector("");
+// const  = document.querySelector("");
+// const  = document.querySelector("");
+// const  = document.querySelector("");
+// const  = document.querySelector("");
+
+// cart
+let cart = [];
+
+// gets the products
+class Products {}
+
+// display products, gets from local storage
+class UI {}
+// local storage
+class Storage {}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const ui = new UI();
+  const products = new Products();
+});
