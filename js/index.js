07bbf6bbd1bdd38677991111168ce2980484da43
Amir-UL <mangoleo51@gmail.com>
2020-11-26 00:48:40 +0600

    added the json file

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
 // getting the DOM
 
-const navbar = document.querySelector(".navbar");
-const closeCartBtn = document.querySelector(".close-cart");
-const clearCartBtn = document.querySelector(".clear-cart");
-const cartDOM = document.querySelector(".card");
-const cartOverlay = document.querySelector(".cart-overlay");
-const cartItems = document.querySelector(".cart-items");
-const cartTotal = document.querySelector(".cart-total");
-const cartContent = document.querySelector(".cart-content");
-const productsDOM = document.querySelector(".products-center");
+const navbar = document.querySelector('.navbar');
+const closeCartBtn = document.querySelector('.close-cart');
+const clearCartBtn = document.querySelector('.clear-cart');
+const cartDOM = document.querySelector('.card');
+const cartOverlay = document.querySelector('.cart-overlay');
+const cartItems = document.querySelector('.cart-items');
+const cartTotal = document.querySelector('.cart-total');
+const cartContent = document.querySelector('.cart-content');
+const productsDOM = document.querySelector('.products-center');
 //
 // const  = document.querySelector("");
 // const  = document.querySelector("");
@@ -20,14 +20,42 @@ const productsDOM = document.querySelector(".products-center");
 let cart = [];
 
 // gets the products
-class Products {}
+class Products {
+  async getProducts() {
+    try {
+      let result = await fetch('../js/products.json');
+      let data = await result.json();
+      let products = data.items;
+      products = products.map((item) => {
+        const { title, price } = item.fields;
+        const { id } = item.sys;
+        const  idc  = item.sys;
+
+        const  image  = item.fields.image.fields.file.url;
+        return {  idc,title, price, id, image };
+      });
+      return products;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
 
 // display products, gets from local storage
-class UI {}
+class UI {
+  displayProducts(products) {
+    console.log(products);
+  }
+}
 // local storage
 class Storage {}
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   const ui = new UI();
   const products = new Products();
+  // get Products
+
+  products.getProducts().then((products) => {
+    ui.displayProducts(products);
+  });
 });
